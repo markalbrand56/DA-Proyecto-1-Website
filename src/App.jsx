2cdcd0773a1cd621f1994bb152d9af500a9e199c
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-16 21:25:20 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TuringTape from './components/TuringTape'
 import './App.css'
 
@@ -20,27 +20,51 @@ function App() {
 
   const [showMachine, setShowMachine] = useState(false)
   const [showAnswer, setShowAnswer] = useState(false)
+  const [turingData, setTuringData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [string , setString] = useState('');
 
-  const onClickMachine = () => {
-    setShowMachine(!showMachine)
-    setShowAnswer(false)
-  }
+  const onClickshowAnswer = async () => {
+    setShowAnswer(true);
+    setShowMachine(false);
+    fetchData();
+  };
 
-  const onClickshowAnswer = () => {
-    setShowAnswer(!showAnswer)
-  }
+  const onChangeString = (e) => {
+    console.log("e.target.value",e.target.value)
+    setString(e.target.value);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://tc-proyecto-3-markalbrand56.koyeb.app/turingMachine/${string}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setTuringData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
 
   return (
     <div className="flex flex-col w-screen h-screen justify-center items-center bg-gray-100">
-      <h1 className='text-4xl text-blue-500 font-bold m-0'> PANAS </h1>
-      <h1 className='text-4xl text-black font-bold'> Turing Machine <span className='text-blue-500'>Simulator</span></h1>
-      {!showMachine && !showAnswer && (<form className='flex flex-col w-1/2 h-1/2 justify-center items-center bg-gray-100'>
+      <div className='flex flex-col w-3/4 justify-start items-center'>
+        <img src="/images/logo.png" alt="Logo-1" border="0" className='w-1/2 h-1/2 object-cover'/>
+      </div>
+      {!showMachine && !showAnswer && (<form className='flex flex-col w-1/2 h-1/2 justify-start items-center bg-gray-100'>
             <label htmlFor="tape" className='text-black'>Cadena</label>
-            <input type="text" id="tape" name="tape" className='w-1/2 bg-black p-2 rounded-md'></input>
+            <input type="text" id="tape" name="tape" className='w-1/2 bg-black p-2 rounded-md' onChange={onChangeString}></input>
             <button type="submit" value="run" className='w-1/2 text-white bg-blue-500 my-8 rounded-md p-2' onClick={onClickshowAnswer}>Run</button>
       </form>)}
-      <button className='h-10 w-10 text-white bg-blue-500 rounded-full fixed bottom-0 right-0 m-4 p-2 hover:bg-blue-700' onClick={onClickMachine}>
+      <button className='h-10 w-10 text-white bg-blue-500 rounded-full fixed bottom-0 right-0 m-4 p-2 hover:bg-blue-700' onClick={() => setShowMachine(!showMachine)}>
       <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7848 0.449982C13.8239 0.449982 14.7167 1.16546 14.9122 2.15495L14.9991 2.59495C15.3408 4.32442 17.1859 5.35722 18.9016 4.7794L19.3383 4.63233C20.3199 4.30175 21.4054 4.69358 21.9249 5.56605L22.7097 6.88386C23.2293 7.75636 23.0365 8.86366 22.2504 9.52253L21.9008 9.81555C20.5267 10.9672 20.5267 13.0328 21.9008 14.1844L22.2504 14.4774C23.0365 15.1363 23.2293 16.2436 22.7097 17.1161L21.925 18.4339C21.4054 19.3064 20.3199 19.6982 19.3382 19.3676L18.9017 19.2205C17.1859 18.6426 15.3408 19.6754 14.9991 21.405L14.9122 21.845C14.7167 22.8345 13.8239 23.55 12.7848 23.55H11.2152C10.1761 23.55 9.28331 22.8345 9.08781 21.8451L9.00082 21.4048C8.65909 19.6754 6.81395 18.6426 5.09822 19.2205L4.66179 19.3675C3.68016 19.6982 2.59465 19.3063 2.07505 18.4338L1.2903 17.1161C0.770719 16.2436 0.963446 15.1363 1.74956 14.4774L2.09922 14.1844C3.47324 13.0327 3.47324 10.9672 2.09922 9.8156L1.74956 9.52254C0.963446 8.86366 0.77072 7.75638 1.2903 6.8839L2.07508 5.56608C2.59466 4.69359 3.68014 4.30176 4.66176 4.63236L5.09831 4.77939C6.81401 5.35722 8.65909 4.32449 9.00082 2.59506L9.0878 2.15487C9.28331 1.16542 10.176 0.449982 11.2152 0.449982H12.7848ZM12 15.3C13.8225 15.3 15.3 13.8225 15.3 12C15.3 10.1774 13.8225 8.69998 12 8.69998C10.1774 8.69998 8.69997 10.1774 8.69997 12C8.69997 13.8225 10.1774 15.3 12 15.3Z" fill="#ffffff"></path> </g></svg>
       </button>
       {showMachine && !showAnswer && (
@@ -50,9 +74,10 @@ function App() {
       )}
       {
         showAnswer && !showMachine && (
-          <div className='flex flex-col w-1/2 h-1/2 justify-start items-center bg-gray-600 rounded-md'>
-          <span className='text-xl text-white font-bold'>aca va la respuesta</span>
-          <TuringTape steps={steps} />
+          <div className='flex flex-col w-1/2 h-1/2 justify-start items-center bg-blue-600 rounded-md'>
+          <span className='text-xl text-white font-bold p-4 uppercase'>Resultado</span>
+          {loading && <p>Cargando...</p>}
+          {turingData && <TuringTape steps={turingData.resultado} />}
           </div>
         )
       }
